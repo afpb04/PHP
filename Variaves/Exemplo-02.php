9721f863d6f452d47a426deed0b5d54f6e06bbef
Antonio Francisco <afpb04@gmail.com>
2018-06-14 19:29:46 -0300

    tipos php

--- Variaves/Exemplo-02.php
+++ Variaves/Exemplo-02.php
@@ -1,4 +1,19 @@
 <?php 
 $anoNascimento = 1995;
-$nomeCompleto = "Antonio Francisco";
+$nome1 = "Antonio Francisco";
+$sobremone = "Pereira Barbosa";
+$nomeCompleto = $nome1 ." ". $sobremone;
+echo($nomeCompleto);
+
+exit;
+echo($nome1);
+echo("<br/>");
+
+//limpando a variavel
+unset($nome1);
+//verificando se a variavel existe
+if(isset($nome1)){
+    echo($nome1);
+}
+
 ?>
