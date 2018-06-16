50268c1c57d5fa36bebcddbbd20ca1fae570a76c
Antonio Francisco <afpb04@gmail.com>
2018-06-15 21:01:36 -0300

    foreach

---
+++ foreach/exemplo-01.php
@@ -0,0 +1,12 @@
+<?php
+$meses = array(
+    "Janeiro","Fervereiro","Marco",
+    "Abril","Maio","Junho",
+    "Julho","Agosto","Setembro",
+    "Outubro","Novembro","Dezenbro"
+);
+foreach($meses as $index => $mes){
+    echo "O indice: ".$index."<br/>";
+    echo "O  mês é: ".$mes. "<br/><br/>";
+}
+?>
