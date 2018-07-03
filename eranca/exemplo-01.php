2998ee81ba0d8b34280eb1636fc4bdb5ee924acd
Antonio <afpb04@hotmail.com>
2018-07-03 20:18:29 -0300

    erança

---
+++ eranca/exemplo-01.php
@@ -0,0 +1,24 @@
+<?php
+
+class Documento{
+    private $numero;
+    public function getNumero(){
+        return $this->numero;
+    }
+    public function setNumero($numero){
+        $this->numero = $numero;
+    }
+}
+
+class CPF extends Documento{
+    public function validar():bool{
+        $numeroCPF = $this->getNumero();
+        //validação do cpf aqui
+        return true;
+    }
+
+}
+$doc = new CPF();
+$doc->setNumero("111222333-44");
+var_dump( $doc->validar());
+?>
