78b4f43bf294d7bb2887f9be221e08465453c9da
Antonio <afpb04@hotmail.com>
2018-07-08 12:51:08 -0300

    consertado o bug

--- pdo/exemplo-04.php
+++ pdo/exemplo-04.php
@@ -1,7 +1,7 @@
 <?php
 $conn = new PDO("mysql:dbname=dbphp7;host=localhost","root", "");
 
-$stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID)");
+$stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
 
 $login = "maria";
 $password ="qwerty";
@@ -11,7 +11,7 @@ $stmt->bindParam(":LOGIN",$login);
 $stmt->bindParam(":PASSWORD",$password);
 $stmt->bindParam(":ID",$id);
 
-$stmt->execute();
+var_dump($stmt->execute());
 
 
 echo "dados atualizados!";
--- pdo/exemplo-05.php
+++ pdo/exemplo-05.php
@@ -1,7 +1,7 @@
 <?php
 $conn = new PDO("mysql:dbname=dbphp7;host=localhost","root", "");
 
-$stmt = $conn->prepare("DELETE FROM tb_usuarios  WHERE idusuario = :ID)");
+$stmt = $conn->prepare("DELETE FROM tb_usuarios  WHERE idusuario = :ID");
 
 $id = 1;
 
