a1f80737b73ae8d3786c5d97ad78c9317514dec3
Antonio <afpb04@hotmail.com>
2018-07-15 20:11:49 -0300

    try catch

---
+++ try/exemplo-02.php
@@ -0,0 +1,24 @@
+<?php 
+
+function trataNome($nome){
+
+    if(!$nome){
+        throw new Exception("Nenhum nome foi informado", 1);
+        
+    }
+    echo ucfirst($nome). "<br/>";
+
+
+}
+try{
+    trataNome("Joao");
+    trataNome("");
+}catch(Exception $e){
+    
+    echo $e->getMessage();
+
+}finally{
+    echo "executou o try!";
+}
+
+?>
