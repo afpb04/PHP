0b70f00ee4ee334a9d703af58566ce85ca63a8ee
Antonio <afpb04@hotmail.com>
2018-07-21 12:20:32 -0300

    gerando thumbnail

--- GD/exemplo-03.php
+++ GD/exemplo-03.php
@@ -5,8 +5,8 @@ $titleColor = imagecolorallocate($image, 0, 0, 0);
 
 $gray = imagecolorallocate($image, 100, 100, 100);
 
-imagettftext($image, 32, 0, 320, 250, $titleColor,"fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf","CERTIFICADO" );
-imagettftext($image, 32, 0, 375, 350,  $titleColor,"fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf","Fatima Barbosa");
+imagettftext($image, 32, 0, 320, 250, $titleColor, "fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf", "CERTIFICADO" );
+imagettftext($image, 32, 0, 375, 350,  $titleColor, "fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf", "Fatima Barbosa");
 imagestring($image, 3, 440, 370, "Concluido em: ".date("d/m/Y"), $titleColor);
 
 header("Content-type: image/jpeg");
