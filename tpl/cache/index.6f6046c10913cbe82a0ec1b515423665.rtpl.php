e36a3634ab74ec4875c1414027fd573f7e9f4cd3
Antonio <afpb04@hotmail.com>
2018-07-22 12:51:23 -0300

    trabalhando com tpl

---
+++ tpl/cache/index.6f6046c10913cbe82a0ec1b515423665.rtpl.php
@@ -0,0 +1,6 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>hello <?php echo htmlspecialchars( $name, ENT_COMPAT, 'UTF-8', FALSE ); ?></h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi delectus consequatur ipsa fuga perspiciatis, ex nesciunt sapiente
+    ea beatae in eaque excepturi reiciendis dolore consectetur eos vitae sint vero. Rerum. Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Neque autem molestiae repellat asperiores corporis libero eaque dolore qui aperiam cum corrupti numquam
+    quaerat dolor, veritatis reiciendis unde beatae quo rem.</p>
+<p><?php echo htmlspecialchars( $version, ENT_COMPAT, 'UTF-8', FALSE ); ?></p>
